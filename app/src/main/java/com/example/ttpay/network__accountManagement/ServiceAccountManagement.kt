61d5932c9124e204810fd05b70dc2fb3da0b67e1
lgrzelj <lgrzelj@student.foi.hr>
2023-11-21 16:17:56 +0100

    Added a POST request method to create new user.  (TT-47)

--- app/src/main/java/com/example/ttpay/network__accountManagement/ServiceAccountManagement.kt
+++ app/src/main/java/com/example/ttpay/network__accountManagement/ServiceAccountManagement.kt
@@ -2,9 +2,15 @@ package com.example.ttpay.network__accountManagement
 
 import retrofit2.Call
 import com.example.ttpay.model.User
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface ServiceAccountManagement {
-    @GET("/api/v1/users")
-    fun getUsers(): Call<List<User>>
+
+    @GET("/api/app/v1/getUsers")
+    fun getAllUsers(): Call<List<User>>
+
+    @POST("/api/app/v1/createUser")  //endpoint for creating new user
+    fun createUser(@Body user: User): Call<User> // Method for sending user data to backend
 }
