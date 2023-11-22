5a431f4f509623852ec3efced8d8936e0e157f59
Ivana Belinić <belinicivana15@gmail.com>
2023-11-22 22:58:42 +0100

    The classes User, UserRole and UserStatus have been updated to match the JSON format in the database. TT-33

--- app/src/main/java/com/example/ttpay/model/User.kt
+++ app/src/main/java/com/example/ttpay/model/User.kt
@@ -1,39 +1,42 @@
 package com.example.ttpay.model
 
+import java.text.SimpleDateFormat
 import java.util.Date
 
 data class User(
-    val id: Int,
-    val firstName: String,
-    val lastName: String,
-    val dateOfBirth: Date,
-    val address: String,
-    val phone: String,
-    val email: String,
+    val id: String,
     val username: String,
     val password: String,
+    val firstName: String,
+    val lastName: String,
+    val email: String,
+    val address: String,
+    val phone: String,
+    val dateOfBirth: String, // Change to String for date format "dd.MM.yyyy"
+    val dateCreated: String, // Change to String for date format "dd.MM.yyyy HH:mm:ss"
+    val dateModified: String, // Change to String for date format "dd.MM.yyyy HH:mm:ss"
     val role: UserRole,
-    val userStatus: UserStatus,
-    val dateCreated: Date,
-    val dateModified: Date
+    val userStatus: UserStatus
 ) {
     companion object {
         // function to create an admin user
         fun createAdmin(): User {
+            val currentDate = SimpleDateFormat("dd.MM.yyyy HH:mm:ss").format(Date())
+
             return User(
-                id = 1,
+                id = "1",
                 firstName = "Admin",
                 lastName = "Admin",
-                dateOfBirth = Date(),
-                address = "Zrmanjska ulica",
+                dateOfBirth = "01.01.1990",
+                address = "Zrmanjska",
                 phone = "099709980",
                 email = "admin@example.com",
                 username = "admin",
                 password = "admin",
                 role = UserRole.admin,
                 userStatus = UserStatus.active,
-                dateCreated = Date(),
-                dateModified = Date()
+                dateCreated = currentDate,
+                dateModified = currentDate
             )
         }
     }
