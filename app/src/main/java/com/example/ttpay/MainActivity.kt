828d8c7aa23cc6ad8014e945ecb3bb73b6cd46ed
lgrzelj <100687117+lgrzelj@users.noreply.github.com>
2023-11-22 20:59:29 +0100

    Revert "Retrofit setup"

--- app/src/main/java/com/example/ttpay/MainActivity.kt
+++ app/src/main/java/com/example/ttpay/MainActivity.kt
@@ -1,21 +1,11 @@
 package com.example.ttpay
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val loginButton: Button = findViewById(R.id.btn_login)
-
-        loginButton.setOnClickListener {
-            // Open LoginActivity when the button is clicked
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-        }
     }
 }
