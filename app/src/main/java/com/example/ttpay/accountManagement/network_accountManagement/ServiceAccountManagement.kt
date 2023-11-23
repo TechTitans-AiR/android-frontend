ef86621f4998ff9ab73a0a05e0988c6445290553
lgrzelj <lgrzelj@student.foi.hr>
2023-11-23 12:49:19 +0100

    Post method added in ServiceAccountManagement. (TT-39)

--- app/src/main/java/com/example/ttpay/accountManagement/network_accountManagement/ServiceAccountManagement.kt
+++ app/src/main/java/com/example/ttpay/accountManagement/network_accountManagement/ServiceAccountManagement.kt
@@ -2,10 +2,15 @@ package com.example.ttpay.accountManagement.network_accountManagement
 
 import retrofit2.Call
 import com.example.ttpay.model.User
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface ServiceAccountManagement {
 
     @GET("/api/v1/users")
     fun getUsers(): Call<List<User>>
+
+    @POST("/api/app/v1/createUser")  //endpoint for creating new user
+    fun createUser(@Body user: User): Call<User> // Method for sending user data to backend
 }
