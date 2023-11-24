8756ddefbae972d25cc74b1509ed4de157ffb27d
Ivana Belinić <belinicivana15@gmail.com>
2023-11-24 12:23:15 +0100

    The endpoint for fetch user details is added. Also, the layout activity_details_merchant is implemented. TT-45

--- app/src/main/java/com/example/ttpay/accountManagement/network_accountManagement/ServiceAccountManagement.kt
+++ app/src/main/java/com/example/ttpay/accountManagement/network_accountManagement/ServiceAccountManagement.kt
@@ -6,11 +6,15 @@ import com.example.ttpay.model.newUser
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface ServiceAccountManagement {
 
     @GET("/api/v1/users")
     fun getUsers(): Call<List<User>>
+
+    @GET("/api/v1/users/{id}")
+    fun getUserDetails(@Path("id") userId: String): Call<User>
 
     @POST("/api/v1/users/create")  //endpoint for creating new user
     fun createNewUser(@Body user: newUser): Call<newUser> // Method for sending user data to backend
