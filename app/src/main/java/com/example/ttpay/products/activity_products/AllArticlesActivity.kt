a6644932f9e1aeb6f5a3bc78a5ff5bfcc505df11
Ivana Belinić <belinicivana15@gmail.com>
2023-11-24 07:32:52 +0100

    Initial implementation for AllArticlesActivity is done. TT-50

--- app/src/main/java/com/example/ttpay/products/activity_products/AllArticlesActivity.kt
+++ app/src/main/java/com/example/ttpay/products/activity_products/AllArticlesActivity.kt
@@ -1,12 +1,37 @@
 package com.example.ttpay.products.activity_products
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.ProgressBar
 import com.example.ttpay.R
+import com.example.ttpay.model.NavigationHandler
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class AllArticlesActivity : AppCompatActivity() {
+
+    private lateinit var navigationHandler: NavigationHandler
+    private lateinit var progressBar: ProgressBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_all_articles)
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        navigationHandler = NavigationHandler(this)
+        navigationHandler.setupWithBottomNavigation(bottomNavigationView)
+        bottomNavigationView.visibility = View.VISIBLE
+
+        progressBar = findViewById(R.id.loadingProgressBar)
+
+        val btnBack: ImageView = findViewById(R.id.imgView_back)
+
+        btnBack.setOnClickListener {
+            val intent = Intent(this, AllProductsActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
