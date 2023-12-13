ecd709e47e84d5d46c4a058f348f33e4cbd7bba5
Ivana Belinić <belinicivana15@gmail.com>
2023-12-13 13:44:08 +0100

    Test 2. TT-74

--- app/src/main/java/com/example/ttpay/transactions/activity_transactions/PaymentOptionsActivity.kt
+++ app/src/main/java/com/example/ttpay/transactions/activity_transactions/PaymentOptionsActivity.kt
@@ -8,10 +8,19 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.RadioGroup
+import androidx.appcompat.app.AlertDialog
 import com.example.ttpay.R
+import com.example.ttpay.accountManagement.network_accountManagement.ServiceAccountManagement
 import com.example.ttpay.model.NavigationHandler
+import com.example.ttpay.model.User
+import com.example.ttpay.network.RetrofitClient
+import com.example.ttpay.sellingItems.network_sellingItems.ServiceTransaction_SellingItems
+import com.example.ttpay.transactions.network_transactions.NewTransaction
 import com.example.ttpay.transactions.network_transactions.ShoppingCartItem
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class PaymentOptionsActivity : AppCompatActivity() {
 
@@ -19,9 +28,10 @@ class PaymentOptionsActivity : AppCompatActivity() {
     private lateinit var shoppingCartItems: MutableList<ShoppingCartItem>
     private var totalAmount: Double = 0.0
     private lateinit var navigationHandler: NavigationHandler
-    private lateinit var complete_payment: Button
+    private lateinit var completePayment: Button
     private lateinit var radioGroupPaymentOptions: RadioGroup
     private lateinit var edtCashAmount: EditText
+    private lateinit var userId: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,16 +51,115 @@ class PaymentOptionsActivity : AppCompatActivity() {
         navigationHandler.setupWithBottomNavigation(bottomNavigationView)
         bottomNavigationView.visibility = View.VISIBLE
 
-        radioGroupPaymentOptions = findViewById(com.example.ttpay.R.id.radioGroupPaymentOptions)
+        radioGroupPaymentOptions = findViewById(R.id.radioGroupPaymentOptions)
         edtCashAmount = findViewById(R.id.edt_cash_amount)
 
-        complete_payment = findViewById(R.id.btn_complete_payment)
-        complete_payment.setOnClickListener {
-            val intent = Intent(this, TransactionCompletionActivity::class.java)
-            intent.putExtra("shoppingCartItems", ArrayList(shoppingCartItems))
-            intent.putExtra("totalAmount", totalAmount)
-            intent.putExtra("username", userUsername)
-            startActivity(intent)
+        completePayment = findViewById(R.id.btn_complete_payment)
+        completePayment.setOnClickListener {
+            handleCompletePayment()
         }
+    }
+
+    private fun handleCompletePayment() {
+        // Dohvati merchantId
+        fetchUserId(userUsername) { merchantId ->
+            // Koristi merchantId
+            val description = "Transaction"
+            val currency = "EUR"
+
+            // Kreiraj objekt NewTransaction
+            val newTransaction = NewTransaction(merchantId, description, totalAmount, currency)
+
+            // Pošalji transakciju na backend
+            sendTransactionToBackend(newTransaction)
+        }
+    }
+
+    private fun fetchUserId(username: String, callback: (String) -> Unit) {
+        val retrofit = RetrofitClient.getInstance(8080)
+        val service = retrofit.create(ServiceAccountManagement::class.java)
+
+        val call = service.getUsers()
+
+        call.enqueue(object : Callback<List<User>> {
+            override fun onResponse(call: Call<List<User>>, response: Response<List<User>>) {
+                if (response.isSuccessful) {
+                    val users = response.body()
+                    val user = users?.find { it.username == username }
+                    if (user != null) {
+                        val merchantId = user.id!!
+                        Log.d("AllCatalogsMerchant", "Fetched merchant ID: $merchantId")
+
+                        // Pozovi callback funkciju s dobivenim merchantId
+                        callback.invoke(merchantId)
+                    } else {
+                        showErrorDialog(userUsername)
+                    }
+                } else {
+                    showErrorDialog(userUsername)
+                }
+            }
+
+            override fun onFailure(call: Call<List<User>>, t: Throwable) {
+                showErrorDialog(userUsername)
+            }
+        })
+    }
+    private fun showErrorDialog(username: String) {
+        val builder = AlertDialog.Builder(this)
+
+        builder.setTitle("Error")
+            .setMessage("Error fetching data.")
+            .setPositiveButton("Retry") { _, _ ->
+                fetchUserId(username) { merchantId ->
+                    // Ovdje možete dodati dodatnu logiku ako je potrebno
+                }
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }
+
+        val dialog = builder.create()
+        dialog.show()
+    }
+
+
+    private fun sendTransactionToBackend(newTransaction: NewTransaction) {
+        val retrofit = RetrofitClient.getInstance(8080)
+        val service = retrofit.create(ServiceTransaction_SellingItems::class.java)
+
+        val call = service.createTransaction(newTransaction)
+
+        call.enqueue(object : Callback<NewTransaction> {
+            override fun onResponse(
+                call: Call<NewTransaction>,
+                response: Response<NewTransaction>
+            ) {
+                if (response.isSuccessful) {
+                    // Uspješno kreirana transakcija
+                    val newTransactionResponse = response.body()
+                    if (newTransactionResponse != null) {
+                        handleTransactionCreationSuccess(newTransactionResponse)
+                    }
+                } else {
+                    // Pogreška prilikom kreiranja transakcije
+                    // Ovdje možete obraditi pogrešku
+                }
+            }
+
+            override fun onFailure(call: Call<NewTransaction>, t: Throwable) {
+                // Greška prilikom komunikacije s backendom
+                // Ovdje možete obraditi grešku
+            }
+        })
+    }
+
+    private fun handleTransactionCreationSuccess(newTransaction: NewTransaction) {
+        val intent = Intent(this, TransactionCompletionActivity::class.java)
+        intent.putExtra("newTransaction", newTransaction)
+        intent.putExtra("shoppingCartItems", ArrayList(shoppingCartItems))
+        intent.putExtra("totalAmount", totalAmount)
+        intent.putExtra("username", userUsername)
+        startActivity(intent)
     }
 }
