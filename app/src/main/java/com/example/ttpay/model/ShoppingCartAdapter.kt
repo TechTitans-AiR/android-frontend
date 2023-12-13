b223186f8be30589a937bf1cd587e1b7db1e4e47
Ivana Belinić <belinicivana15@gmail.com>
2023-12-13 09:00:45 +0100

    The adapter ShoppingCartAdapter is implemented. Also, the ShoppingCartItem class is created. TT-74

---
+++ app/src/main/java/com/example/ttpay/model/ShoppingCartAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.ttpay.model
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.ttpay.R
+import com.example.ttpay.transactions.network_transactions.ShoppingCartItem
+
+class ShoppingCartAdapter(private val items: List<ShoppingCartItem>) : RecyclerView.Adapter<ShoppingCartAdapter.ViewHolder>() {
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val productName: TextView = itemView.findViewById(R.id.tv_product_name)
+        val quantity: TextView = itemView.findViewById(R.id.tv_quantity)
+        val price: TextView = itemView.findViewById(R.id.tv_price)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_shopping_cart, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = items[position]
+
+        holder.productName.text = item.name
+        holder.quantity.text = item.quantity.toString()
+        holder.price.text = "€${item.price}"
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+}
---
+++ app/src/main/java/com/example/ttpay/transactions/network_transactions/ShoppingCartItem.kt
@@ -0,0 +1,7 @@
+package com.example.ttpay.transactions.network_transactions
+
+data class ShoppingCartItem(
+    val name: String,
+    val quantity: Int,
+    val price: Double
+)
