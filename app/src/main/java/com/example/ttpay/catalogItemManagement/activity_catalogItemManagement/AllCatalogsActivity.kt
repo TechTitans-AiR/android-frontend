2ddb2797d9f92d12074b827965439f8f59faa3cc
Ivana Belinić <belinicivana15@gmail.com>
2023-12-28 23:50:43 +0100

    The activity for displaying all catalogs has been modified, and now it automatically retrieves all catalogs without the previous step of fetching users first and then catalogs. Additionally, the clickable text view for catalogs without users has been removed.

--- app/src/main/java/com/example/ttpay/catalogItemManagement/activity_catalogItemManagement/AllCatalogsActivity.kt
+++ app/src/main/java/com/example/ttpay/catalogItemManagement/activity_catalogItemManagement/AllCatalogsActivity.kt
@@ -21,6 +21,7 @@ import com.example.ttpay.accountManagement.network_accountManagement.ServiceAcco
 import com.example.ttpay.catalogItemManagement.activity_catalogItemManagement.activity_createCatalogItem.CreateCatalogItemActivity
 import com.example.ttpay.catalogItemManagement.network_catalogItemManagement.ServiceCatalogItemManagement
 import com.example.ttpay.model.Catalog
+import com.example.ttpay.model.CatalogAdapter
 import com.example.ttpay.model.NavigationHandler
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import retrofit2.Call
@@ -31,8 +32,8 @@ class AllCatalogsActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var navigationHandler: NavigationHandler
     private lateinit var userUsername: String
-    private val adapter = UserAdapter(emptyList()) { user ->
-        openCatalogItemActivity(user.id)
+    private val catalogAdapter = CatalogAdapter(emptyList()) { catalog ->
+        openDetailedCatalogItemActivity(catalog.id)
     }
     private lateinit var progressBar: ProgressBar
 
@@ -42,9 +43,9 @@ class AllCatalogsActivity : AppCompatActivity() {
 
         userUsername = intent.getStringExtra("username") ?: ""
 
-        recyclerView = findViewById(R.id.recyclerView_all_users)
+        recyclerView = findViewById(R.id.recyclerView_all_catalogs)
         recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = adapter
+        recyclerView.adapter = catalogAdapter
 
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
         navigationHandler = NavigationHandler(this, userUsername)
@@ -53,7 +54,7 @@ class AllCatalogsActivity : AppCompatActivity() {
 
         progressBar = findViewById(R.id.loadingProgressBar)
 
-        fetchUsers()
+        fetchCatalogs()
 
         val imgBack: ImageView = findViewById(R.id.back_button)
         imgBack.setOnClickListener {
@@ -61,11 +62,6 @@ class AllCatalogsActivity : AppCompatActivity() {
             intent.putExtra("username", userUsername)
             startActivity(intent)
             finish()
-        }
-
-        val textViewCatalogName: TextView = findViewById(R.id.textViewCatalogName)
-        textViewCatalogName.setOnClickListener {
-            openCatalogsWithoutUsersActivity()
         }
     }
 
@@ -76,33 +72,33 @@ class AllCatalogsActivity : AppCompatActivity() {
         finish()
     }
 
-    private fun openCatalogItemActivity(userId: String?) {
-        val intent = Intent(this, CatalogItemActivity::class.java)
-        intent.putExtra("userId", userId)
+    private fun openDetailedCatalogItemActivity(catalogId: String?) {
+        val intent = Intent(this, DetailedCatalogItemActivity::class.java)
+        intent.putExtra("catalogId", catalogId)
         intent.putExtra("username", userUsername)
         startActivity(intent)
     }
 
-    private fun fetchUsers() {
+    private fun fetchCatalogs() {
         showLoading()
-        val retrofit = RetrofitClient.getInstance(8080)//za account_management
-        val service = retrofit.create(ServiceAccountManagement::class.java)
-        val call = service.getUsers()
+        val retrofit = RetrofitClient.getInstance(8081)
+        val service = retrofit.create(ServiceCatalogItemManagement::class.java)
+        val call = service.getCatalogs()
 
-        call.enqueue(object : Callback<List<User>> {
-            override fun onResponse(call: Call<List<User>>, response: Response<List<User>>) {
-                Log.d("Poruka: ", response.code().toString())
+        call.enqueue(object : Callback<List<Catalog>> {
+            override fun onResponse(call: Call<List<Catalog>>, response: Response<List<Catalog>>) {
+                Log.d("AllCatalogsActivity", "Response code: ${response.code()}")
                 hideLoading()
                 if (response.isSuccessful) {
-                    val users = response.body() ?: emptyList()
-                    Log.d("AllCatalogsActivity", "Users fetched successfully: $users")
-                    adapter.updateData(users)
+                    val catalogs = response.body() ?: emptyList()
+                    Log.d("AllCatalogsActivity", "Catalogs fetched successfully: $catalogs")
+                    catalogAdapter.updateData(catalogs)
                 } else {
                     showErrorDialog()
                 }
             }
 
-            override fun onFailure(call: Call<List<User>>, t: Throwable) {
+            override fun onFailure(call: Call<List<Catalog>>, t: Throwable) {
                 hideLoading()
                 showErrorDialog()
             }
@@ -121,9 +117,9 @@ class AllCatalogsActivity : AppCompatActivity() {
         val builder = AlertDialog.Builder(this)
 
         builder.setTitle("Error")
-            .setMessage("Error fetching data.")
+            .setMessage("Error fetching catalogs.")
             .setPositiveButton("Retry") { _, _ ->
-                fetchUsers()
+                fetchCatalogs()
             }
             .setNegativeButton("Cancel") { dialog, _ ->
                 dialog.dismiss()
@@ -133,9 +129,4 @@ class AllCatalogsActivity : AppCompatActivity() {
         dialog.show()
     }
 
-    private fun openCatalogsWithoutUsersActivity() {
-        val intent = Intent(this, CatalogItemWithoutUserActivity::class.java)
-        intent.putExtra("username", userUsername)
-        startActivity(intent)
-    }
 }
