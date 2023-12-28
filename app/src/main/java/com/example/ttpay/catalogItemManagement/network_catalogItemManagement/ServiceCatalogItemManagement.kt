2b32efe8246cad72a93d30bf596ea3de375a3fa3
Ivana Belinić <belinicivana15@gmail.com>
2023-12-28 11:51:43 +0100

    The ServiceCatalogItemManagement has been updated, and endpoints for disabling and enabling the catalog have been added. TT-96

--- app/src/main/java/com/example/ttpay/catalogItemManagement/network_catalogItemManagement/ServiceCatalogItemManagement.kt
+++ app/src/main/java/com/example/ttpay/catalogItemManagement/network_catalogItemManagement/ServiceCatalogItemManagement.kt
@@ -6,6 +6,7 @@ import com.example.ttpay.model.User
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.PATCH
 import retrofit2.http.POST
 import retrofit2.http.Path
 
@@ -22,6 +23,12 @@ interface ServiceCatalogItemManagement {
 
     @GET("/api/v1/catalogs")
     fun getCatalogs(): Call<List<Catalog>>
+
+    @PATCH("/api/v1/catalogs/disable/{catalogId}")
+    fun disableCatalog(@Path("catalogId") catalogId: String): Call<Catalog>
+
+    @PATCH("/api/v1/catalogs/enable/{catalogId}")
+    fun enableCatalog(@Path("catalogId") catalogId: String): Call<Catalog>
 
 
 }
