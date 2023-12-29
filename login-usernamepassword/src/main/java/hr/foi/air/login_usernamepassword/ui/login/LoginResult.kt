17e5977e4a458cf152b7b1d9e187b921ccd73c43
lgrzelj <lgrzelj@student.foi.hr>
2023-12-29 21:51:57 +0100

    Changed packages in module for login with username and password. (TT-101)

--- login-usernamepassword/src/main/java/hr/foi/air/login_usernamepassword/ui/login/LoginResult.kt
+++
@@ -1,9 +0,0 @@
-package hr.foi.air.login_usernamepassword.ui.login
-
-/**
- * Authentication result : success (user details) or error message.
- */
-data class LoginResult(
-    val success: LoggedInUserView? = null,
-    val error: Int? = null
-)
