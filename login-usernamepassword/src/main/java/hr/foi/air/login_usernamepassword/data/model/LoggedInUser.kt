e657c4b156ad7ce640e51ac6b216e3a90162ff61
lgrzelj <lgrzelj@student.foi.hr>
2023-12-29 20:08:16 +0100

    Created new module for Login with username and password, module type is Login view activity. (TT-101)

---
+++ login-usernamepassword/src/main/java/hr/foi/air/login_usernamepassword/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package hr.foi.air.login_usernamepassword.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)
---
+++ login-usernamepassword/src/main/java/hr/foi/air/login_usernamepassword/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package hr.foi.air.login_usernamepassword.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
