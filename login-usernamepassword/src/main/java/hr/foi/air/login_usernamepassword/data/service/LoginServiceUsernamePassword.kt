aba1e6a02fb705ece83beac3e890fecd3738f21b
lgrzelj <lgrzelj@student.foi.hr>
2023-12-29 23:34:44 +0100

    Accidentaly left dependecy for app module. (TT-101)

--- login-usernamepassword/src/main/java/hr/foi/air/login_usernamepassword/data/service/LoginServiceUsernamePassword.kt
+++ login-usernamepassword/src/main/java/hr/foi/air/login_usernamepassword/data/service/LoginServiceUsernamePassword.kt
@@ -2,7 +2,6 @@ package hr.foi.air.login_usernamepassword.data.service
 
 import hr.foi.air.login_usernamepassword.data.model.LoginRequestData
 import hr.foi.air.login_usernamepassword.data.model.LoginResponseData
-import hr.foi.techtitans.ttpay.navigationBar.activity_navigationBar.login.LoginRequest
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.POST
