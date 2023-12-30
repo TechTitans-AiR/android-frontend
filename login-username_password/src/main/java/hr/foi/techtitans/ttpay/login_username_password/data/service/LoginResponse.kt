80b771876c1cc06dcf3cff47f94c756a582f13a0
lgrzelj <lgrzelj@foi.hr>
2023-12-30 15:59:42 +0100

    LoginResponse is responsible for holding data that API sends like response for current user which is trying to log in. (TT-101)

---
+++ login-username_password/src/main/java/hr/foi/techtitans/ttpay/login_username_password/data/service/LoginResponse.kt
@@ -0,0 +1,13 @@
+package hr.foi.techtitans.ttpay.login_username_password.data.service
+
+data class LoginResponse(
+    val headers: Map<String, String>,
+    val body: Body,
+    val statusCode: String,
+    val statusCodeValue: Int
+)
+
+data class Body(
+    val message: String,
+    val token: String
+)
