028e3330a97f5e36f2ff5d527bbce452c350aefb
lgrzelj <lgrzelj@foi.hr>
2023-12-30 15:44:36 +0100

    Created new module for login with username and password, this time module type is Empty Vies Activity. (TT-101)

---
+++ login-username_password/src/main/java/hr/foi/techtitans/ttpay/login_username_password/LoginActivityUsernamePassword.kt
@@ -0,0 +1,11 @@
+package hr.foi.techtitans.ttpay.login_username_password
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivityUsernamePassword : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_activity_username_password)
+    }
+}
