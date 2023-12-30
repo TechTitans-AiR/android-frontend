b3a80dbed0cb10a291f6f12a2700d0f0b8ed2b71
lgrzelj <lgrzelj@student.foi.hr>
2023-12-30 13:32:48 +0100

    Tried to changed activity or something maybe there where problem but unfortunately still occurs error. So with this branch is finished for now. Modify featue will be implemented on another branch. (TT-101)

--- app/src/main/java/hr/foi/techtitans/ttpay/navigationBar/activity_navigationBar/MainActivity.kt
+++ app/src/main/java/hr/foi/techtitans/ttpay/navigationBar/activity_navigationBar/MainActivity.kt
@@ -1,9 +1,9 @@
 package hr.foi.techtitans.ttpay.navigationBar.activity_navigationBar
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import hr.foi.techtitans.ttpay.R
 
 class MainActivity : AppCompatActivity() {
@@ -14,7 +14,12 @@ class MainActivity : AppCompatActivity() {
         val loginButton: Button = findViewById(R.id.btn_login)
 
         loginButton.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
+            /*val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent) */
+            val intent = Intent(
+                this,
+                hr.foi.air.login_usernamepassword.ui.login.LoginUsernamePassword::class.java
+            )
             startActivity(intent)
         }
     }
