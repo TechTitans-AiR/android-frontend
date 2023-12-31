67b4fd91318fa3f4c9cbccacad8ef21f6d5886c2
lgrzelj <lgrzelj@foi.hr>
2023-12-31 01:49:10 +0100

    Updated MainActivity for buttons each for one of the following logins. (TT-101)

--- app/src/main/java/hr/foi/techtitans/ttpay/navigationBar/activity_navigationBar/MainActivity.kt
+++ app/src/main/java/hr/foi/techtitans/ttpay/navigationBar/activity_navigationBar/MainActivity.kt
@@ -5,17 +5,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import hr.foi.techtitans.ttpay.R
+import hr.foi.techtitans.ttpay.login_username_password.LoginActivityUsernamePassword
+import hr.foi.techtitans.ttpay.login_username_password.data.LoggedInUser
 
 class MainActivity : AppCompatActivity() {
+
+    private val LOGIN_REQUEST_CODE = 123 // Bilo koji broj koji odgovara tvojim potrebama
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val loginButton: Button = findViewById(R.id.btn_login)
+        val loginButtonUsernamePassword: Button = findViewById(R.id.btn_login)
+        val loginButtonFingertip:Button=findViewById(R.id.btn_login2)
 
-        loginButton.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
+
+        loginButtonUsernamePassword.setOnClickListener {
+            val loginIntent = Intent(this, LoginActivityUsernamePassword::class.java)
+            startActivityForResult(loginIntent, LOGIN_REQUEST_CODE)
+            val receivedUser: LoggedInUser? = intent.getParcelableExtra("loggedInUser")
+        }
+        loginButtonFingertip.setOnClickListener{
+            //TODO: intent for activity for fingertip
         }
     }
 }
