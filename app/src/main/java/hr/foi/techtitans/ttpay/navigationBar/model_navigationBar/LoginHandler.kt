f8a3015ce35b6ee7782aed87a4b886f56dd0c9c9
lgrzelj <lgrzelj@student.foi.hr>
2024-01-03 14:20:29 +0100

    Basic method for handling login are created in LoginHandler interface. (TT-101)

--- app/src/main/java/hr/foi/techtitans/ttpay/navigationBar/model_navigationBar/LoginHandler.kt
+++ app/src/main/java/hr/foi/techtitans/ttpay/navigationBar/model_navigationBar/LoginHandler.kt
@@ -1,4 +1,19 @@
 package hr.foi.techtitans.ttpay.navigationBar.model_navigationBar
 
+import hr.foi.techtitans.ttpay.accountManagement.model_accountManagement.User
+
+
+
+
 interface LoginHandler {
+    //handler which will be used for every login module
+
+    fun performLogin(callback: LoginCallback?)
+
+    //TODO: add storing user data method
+}
+
+interface LoginCallback {
+    fun onLoginSuccess(user: User?)
+    fun onLoginFailure(errorMessage: String?)
 }
