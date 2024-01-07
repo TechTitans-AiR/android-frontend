f508bbeadbed24d3f22f96c6e46c8384350d938d
ibelinic <ibelinic20@foi.hr>
2024-01-07 15:54:46 +0100

    Declaration and initialization of UI elements. TT-121

--- app/src/main/java/hr/foi/techtitans/ttpay/products/activity_products/DetailsServiceActivity.kt
+++ app/src/main/java/hr/foi/techtitans/ttpay/products/activity_products/DetailsServiceActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import hr.foi.techtitans.ttpay.R
 import hr.foi.techtitans.ttpay.login_modular.model_login.LoggedInUser
@@ -16,11 +18,35 @@ class DetailsServiceActivity : AppCompatActivity() {
     private lateinit var loggedInUser: LoggedInUser
     private lateinit var navigationHandler: NavigationHandler
 
+    //UI elements to display service details
+    private lateinit var serviceNameTextView: TextView
+    private lateinit var descriptionTextView: TextView
+    private lateinit var serviceProviderTextView: TextView
+    private lateinit var priceTextView: TextView
+    private lateinit var currencyTextView: TextView
+    private lateinit var durationTextView: TextView
+    private lateinit var durationUnitTextView: TextView
+    private lateinit var availabilityTextView: TextView
+    private lateinit var locationTextView: TextView
+    private lateinit var progressBar: ProgressBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details_service)
 
         val B_back : ImageView = findViewById(R.id.back_ac)
+
+        // Initialization of UI elements
+        serviceNameTextView = findViewById(R.id.serviceNameTextView)
+        descriptionTextView = findViewById(R.id.descriptionTextView)
+        serviceProviderTextView = findViewById(R.id.serviceProviderTextView)
+        priceTextView = findViewById(R.id.priceTextView)
+        currencyTextView = findViewById(R.id.currencyTextView)
+        durationTextView = findViewById(R.id.durationTextView)
+        durationUnitTextView = findViewById(R.id.durationUnitTextView)
+        availabilityTextView = findViewById(R.id.availabilityTextView)
+        locationTextView = findViewById(R.id.locationTextView)
+        progressBar = findViewById(R.id.progressBar)
 
         loggedInUser = intent.getParcelableExtra("loggedInUser")!!
         userUsername = intent.getStringExtra("username") ?: ""
