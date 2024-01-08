163c058f692cf7e506a58784f5e338758fdf78f3
ibelinic <ibelinic20@foi.hr>
2024-01-08 14:53:15 +0100

    The getServiceDetails method has been implemented to retrieve details about a specific service based on the service ID. Additionally, the updateUIWithServiceDetails method has been implemented to set the retrieved data into specific text views and edit text fields. TT-110.

--- app/src/main/java/hr/foi/techtitans/ttpay/products/activity_products/UpdateServiceActivity.kt
+++ app/src/main/java/hr/foi/techtitans/ttpay/products/activity_products/UpdateServiceActivity.kt
@@ -8,10 +8,18 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.ProgressBar
+import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import hr.foi.techtitans.ttpay.R
 import hr.foi.techtitans.ttpay.login_modular.model_login.LoggedInUser
 import hr.foi.techtitans.ttpay.navigationBar.model_navigationBar.NavigationHandler
+import hr.foi.techtitans.ttpay.network.RetrofitClient
+import hr.foi.techtitans.ttpay.products.model_products.Article
+import hr.foi.techtitans.ttpay.products.model_products.Service
+import hr.foi.techtitans.ttpay.products.network_products.ServiceProducts
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class UpdateServiceActivity : AppCompatActivity() {
 
@@ -72,5 +80,58 @@ class UpdateServiceActivity : AppCompatActivity() {
 
         // Get service details based on the provided serviceId
         serviceId = intent.getStringExtra("serviceId") ?: ""
+        if (serviceId != null) {
+            getServiceDetails(serviceId)
+        }
+    }
+
+    private fun updateUIWithServiceDetails(service: Service?) {
+        // Update the UI elements with the fetched service details
+        if (service != null) {
+            serviceNameEditText.setText(service.serviceName)
+            descriptionEditText.setText(service.description)
+            serviceProviderEditText.setText(service.serviceProvider)
+            priceEditText.setText(service.price.toString())
+            currencyEditText.setText(service.currency)
+            durationEditText.setText(service.duration.toString())
+            durationUnitEditText.setText(service.durationUnit)
+            availabilityEditText.setText(service.availability)
+            locationEditText.setText(service.serviceLocation)
+
+        } else {
+            serviceNameEditText.text.clear()
+            descriptionEditText.text.clear()
+            priceEditText.text.clear()
+            currencyEditText.text.clear()
+            serviceProviderEditText.text.clear()
+            durationEditText.text.clear()
+            durationUnitEditText.text.clear()
+            availabilityEditText.text.clear()
+            locationEditText.text.clear()
+            Toast.makeText(this@UpdateServiceActivity, "Service details not available", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun getServiceDetails(serviceId: String) {
+        val retrofit = RetrofitClient.getInstance(8081)
+        val service = retrofit.create(ServiceProducts::class.java)
+        val call = service.getServiceDetails(serviceId)
+        call.enqueue(object : Callback<Service> {
+            override fun onResponse(call: Call<Service>, response: Response<Service>) {
+                if (response.isSuccessful) {
+                    val service = response.body()
+                    updateUIWithServiceDetails(service)
+                } else {
+                    Toast.makeText(this@UpdateServiceActivity, "Failed to retrieve article details", Toast.LENGTH_SHORT).show()
+                }
+                progressBar.visibility = View.GONE
+            }
+
+            override fun onFailure(call: Call<Service>, t: Throwable) {
+                Log.e("UpdateArticleActivity", "Error: ${t.message}", t)
+                Toast.makeText(this@UpdateServiceActivity, "Error: ${t.message}", Toast.LENGTH_SHORT).show()
+                progressBar.visibility = View.GONE
+            }
+        })
     }
 }
