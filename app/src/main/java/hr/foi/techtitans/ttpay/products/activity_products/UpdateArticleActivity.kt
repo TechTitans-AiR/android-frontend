9b2b19ddda19a46c0be9178d484cab55c00dab95
ibelinic <ibelinic20@foi.hr>
2024-01-08 10:14:39 +0100

    The navigation has been added to the UpdateArticleActivity view and the UpdateServiceActivity view, and the use of this navigation has also been incorporated into those activities. TT-110

--- app/src/main/java/hr/foi/techtitans/ttpay/products/activity_products/UpdateArticleActivity.kt
+++ app/src/main/java/hr/foi/techtitans/ttpay/products/activity_products/UpdateArticleActivity.kt
@@ -3,14 +3,18 @@ package hr.foi.techtitans.ttpay.products.activity_products
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.ImageView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import hr.foi.techtitans.ttpay.R
 import hr.foi.techtitans.ttpay.login_modular.model_login.LoggedInUser
+import hr.foi.techtitans.ttpay.navigationBar.model_navigationBar.NavigationHandler
 
 class UpdateArticleActivity : AppCompatActivity() {
 
     private lateinit var userUsername: String
     private lateinit var loggedInUser: LoggedInUser
+    private lateinit var navigationHandler: NavigationHandler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_update_article)
@@ -19,6 +23,11 @@ class UpdateArticleActivity : AppCompatActivity() {
         userUsername = intent.getStringExtra("username") ?: ""
 
         val btnBack: ImageView = findViewById(R.id.imgView_back)
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        navigationHandler = NavigationHandler(this, loggedInUser)
+        navigationHandler.setupWithBottomNavigation(bottomNavigationView)
+        bottomNavigationView.visibility = View.VISIBLE
 
         btnBack.setOnClickListener {
             intent.putExtra("loggedInUser", loggedInUser)
