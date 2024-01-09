4062b2627480e31debba7ddfe9fd2b18c7073f24
lgrzelj <lgrzelj@student.foi.hr>
2024-01-09 18:03:03 +0100

    ServiceTransactionManagement.kt in methods now contains header for token of logged user.  (TT-91)

--- app/src/main/java/hr/foi/techtitans/ttpay/transactions/network_transactions/ServiceTransactionManagement.kt
+++ app/src/main/java/hr/foi/techtitans/ttpay/transactions/network_transactions/ServiceTransactionManagement.kt
@@ -6,6 +6,7 @@ import hr.foi.techtitans.ttpay.transactions.model_transactions.Transaction
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.POST
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -14,16 +15,24 @@ import retrofit2.http.QueryMap
 interface ServiceTransactionManagement {
 
     @GET("/api/v1/transactions/merchant/{merchantID}")
-    fun getUserTransactions(@Path("merchantID") merchantID: String): Call<List<Transaction>>
+    fun getUserTransactions(
+        @Header("Authorization") token: String,
+        @Path("merchantID") merchantID: String
+    ): Call<List<Transaction>>
 
     @GET("/api/v1/transactions")
-    fun getTransactions(): Call<List<Transaction>>
+    fun getTransactions(
+        @Header("Authorization") token: String
+    ): Call<List<Transaction>>
 
     @POST("/api/v1/transactions/create")//create new transaction for selling items
     fun createTransaction(@Body newTransaction: NewTransaction): Call<NewTransaction>
 
     @GET("/api/v1/transactions/{id}")
-    fun getTransactionDetails(@Path("id") id: String): Call<Transaction>
+    fun getTransactionDetails(
+        @Header("Authorization") token: String,
+        @Path("id") id: String
+    ): Call<Transaction>
 
     @POST("api/v1/transactions/search")
     fun searchTransactions(@Body searchParams: Map<String, String>): Call<List<Transaction>>
