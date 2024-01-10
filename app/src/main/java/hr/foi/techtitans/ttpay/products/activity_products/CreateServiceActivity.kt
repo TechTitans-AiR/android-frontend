aafbf61b21215fe3fb64db5f9d1d5fabe9feaffc
ibelinic <ibelinic20@foi.hr>
2024-01-10 20:44:22 +0100

    Declared and initialized all added elements from the layout. TT-129

--- app/src/main/java/hr/foi/techtitans/ttpay/products/activity_products/CreateServiceActivity.kt
+++ app/src/main/java/hr/foi/techtitans/ttpay/products/activity_products/CreateServiceActivity.kt
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.ProgressBar
+import android.widget.Spinner
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import hr.foi.techtitans.ttpay.R
 import hr.foi.techtitans.ttpay.core.LoggedInUser
@@ -21,6 +24,19 @@ class CreateServiceActivity : AppCompatActivity() {
     private lateinit var userUsername: String
     private lateinit var loggedInUser: LoggedInUser
 
+    // Declare and initialize EditText and Spinner
+    private lateinit var editTextServiceName: EditText
+    private lateinit var editTextDescription: EditText
+    private lateinit var editTextServiceProvider: EditText
+    private lateinit var editTextPrice: EditText
+    private lateinit var spinnerCurrency: Spinner
+    private lateinit var editTextDuration: EditText
+    private lateinit var spinnerDurationUnit: Spinner
+    private lateinit var editTextAvailability: EditText
+    private lateinit var editTextServiceLocation: EditText
+    private lateinit var btnCreateNewService: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_service)
@@ -28,6 +44,19 @@ class CreateServiceActivity : AppCompatActivity() {
         loadingDataProgressBar = findViewById(R.id.loadingData)
         actionBack = findViewById(R.id.action_back)
         bottomNavigationView = findViewById(R.id.bottomNavigationView)
+
+        // Initialize EditText and Spinner
+        editTextServiceName = findViewById(R.id.editTextServiceName)
+        editTextDescription = findViewById(R.id.editTextDescription)
+        editTextServiceProvider = findViewById(R.id.editTextServiceProvider)
+        editTextPrice = findViewById(R.id.editTextPrice)
+        spinnerCurrency = findViewById(R.id.spinnerCurrency)
+        editTextDuration = findViewById(R.id.editTextDuration)
+        spinnerDurationUnit = findViewById(R.id.spinnerDurationUnit)
+        editTextAvailability = findViewById(R.id.editTextAvailability)
+        editTextServiceLocation = findViewById(R.id.editTextServiceLocation)
+        btnCreateNewService = findViewById(R.id.btnCreateNewService)
+
 
         loggedInUser = intent.getParcelableExtra("loggedInUser")!!
         userUsername = intent.getStringExtra("username") ?: ""
