82a43e501e244f93e42a713a605bc5edb679a813
ibelinic <ibelinic20@foi.hr>
2024-01-10 20:31:20 +0100

    Created the CreateServiceActivity activity and renamed the CreateProductActivity to CreateArticleActivity. Also, the corresponding layout for CreateProductActivity has been renamed. TT-129

---
+++ app/src/main/java/hr/foi/techtitans/ttpay/products/activity_products/CreateServiceActivity.kt
@@ -0,0 +1,12 @@
+package hr.foi.techtitans.ttpay.products.activity_products
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import hr.foi.techtitans.ttpay.R
+
+class CreateServiceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_service)
+    }
+}
