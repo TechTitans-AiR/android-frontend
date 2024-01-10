8fdd9261d4d9372ffbeb890943264ae8ac4c19fa
lgrzelj <lgrzelj@student.foi.hr>
2024-01-10 23:05:31 +0100

    Implemented logic for showing value in spinners. (TT-128)

--- app/src/main/java/hr/foi/techtitans/ttpay/products/activity_products/CreateArticleActivity.kt
+++ app/src/main/java/hr/foi/techtitans/ttpay/products/activity_products/CreateArticleActivity.kt
@@ -5,7 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.Spinner
 import hr.foi.techtitans.ttpay.R
 import hr.foi.techtitans.ttpay.navigationBar.model_navigationBar.NavigationHandler
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -13,9 +18,24 @@ import hr.foi.techtitans.ttpay.core.LoggedInUser
 
 class CreateArticleActivity : AppCompatActivity() {
 
+    private lateinit var loadingDataProgressBar: ProgressBar
+    private lateinit var actionBack: ImageView
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     private lateinit var navigationHandler: NavigationHandler
     private lateinit var userUsername: String
     private lateinit var loggedInUser: LoggedInUser
+
+    // Declare and initialize EditText and Spinner
+    private lateinit var editTextArticleName: EditText
+    private lateinit var editTextDescription: EditText
+    private lateinit var editTextBrand: EditText
+    private lateinit var editTextPrice: EditText
+    private lateinit var spinnerCurrency: Spinner
+    private lateinit var editTextWeight: EditText
+    private lateinit var spinnerCategory: Spinner
+    private lateinit var editTextQuantityInStock : EditText
+    private lateinit var btnCreateArticle:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,5 +57,45 @@ class CreateArticleActivity : AppCompatActivity() {
             intent.putExtra("username", userUsername)
             startActivity(intent)
         }
+
+
+        //initializing fields
+        editTextArticleName= findViewById(R.id.editTextArticleName)
+        editTextDescription= findViewById(R.id.editTextDescription)
+        editTextBrand= findViewById(R.id.editTextArticleBrand)
+        editTextPrice= findViewById(R.id.editTextPrice)
+        editTextWeight= findViewById(R.id.editTextWeight)
+        editTextQuantityInStock = findViewById(R.id.editTextQuantityInStock)
+        spinnerCategory=findViewById(R.id.spinnerCategory)
+        spinnerCurrency=findViewById(R.id.spinnerCurrency)
+        btnCreateArticle=findViewById(R.id.btnCreateNewArticle)
+
+        //set spinners
+        setupCurrencySpinner()
+        setupCategorySpinner()
+
+        //TODO: implement method for calling endpoint
+
+    }
+
+    private fun setupCurrencySpinner() {
+        val currencies = arrayOf("USD", "EUR", "GBP", "JPY", "CAD", "AUD", "CNY", "INR") // Add more currencies as needed
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, currencies)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerCurrency.adapter = adapter
+    }
+    private fun setupCategorySpinner() {
+        val categories = arrayOf("Fruit", "Kitchenware") // Add more currencies as needed
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, categories)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerCategory.adapter=adapter
+    }
+
+    private fun showLoading() {
+        loadingDataProgressBar?.visibility = View.VISIBLE
+    }
+
+    private fun hideLoading() {
+        loadingDataProgressBar?.visibility = View.GONE
     }
 }
