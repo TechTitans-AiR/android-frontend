e12e7b24b372f6b06a1d2ee0394094962bb1c5a7
ibelinic <ibelinic20@foi.hr>
2024-01-11 23:12:16 +0100

    Created Card class with added format checks for cardNumber, expirationDate, and cvc. TT-100

---
+++ app/src/main/java/hr/foi/techtitans/ttpay/transactions/model_transactions/Card.kt
@@ -0,0 +1,22 @@
+package hr.foi.techtitans.ttpay.transactions.model_transactions
+
+data class Card(
+    val cardNumber: String,
+    val expirationDate: String,
+    val balance: Int,
+    val cvc: Int
+) {
+    init {
+        require(cardNumber.matches(Regex("\\d{4} \\d{4} \\d{4} \\d{4}"))) {
+            "Card number must be in the format 'zzzz zzzz zzzz zzzz', where z is a digit."
+        }
+
+        require(expirationDate.matches(Regex("\\d{4}-\\d{2}"))) {
+            "Expiration date must be in the format 'yyyy-mm'."
+        }
+
+        require(cvc.toString().length == 3) {
+            "CVC must have exactly three numbers."
+        }
+    }
+}
