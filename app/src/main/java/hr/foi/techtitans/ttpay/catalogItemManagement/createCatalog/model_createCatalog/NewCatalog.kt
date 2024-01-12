62284dea3fb47fa0ad02cbfd2559544dc2c24dc7
lgrzelj <lgrzelj@student.foi.hr>
2024-01-12 02:41:09 +0100

    Added nes data class for updating catalog which contains list of strings for articles, users and services and also catalog id. (TT-108)

--- app/src/main/java/hr/foi/techtitans/ttpay/catalogItemManagement/createCatalog/model_createCatalog/NewCatalog.kt
+++ app/src/main/java/hr/foi/techtitans/ttpay/catalogItemManagement/createCatalog/model_createCatalog/NewCatalog.kt
@@ -7,3 +7,12 @@ data class NewCatalog(
     val users:List<String>?,
     var disabled: Boolean
 )
+
+data class UpdateCatalog(
+    val id: String?,
+    val name: String,
+    val articles:List<String>,
+    val services:List<String>,
+    val users:List<String>?,
+    var disabled: Boolean
+)
