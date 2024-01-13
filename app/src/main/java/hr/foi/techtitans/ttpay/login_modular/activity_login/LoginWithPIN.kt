736c4a8ca72acd397b7a1a15cffe86999586c2b6
lgrzelj <lgrzelj@student.foi.hr>
2024-01-13 23:51:28 +0100

    Renamed button for login and added variables that will be used for layout elements, like editText, buttons. (TT-93)

---
+++ app/src/main/java/hr/foi/techtitans/ttpay/login_modular/activity_login/LoginWithPIN.kt
@@ -0,0 +1,42 @@
+package hr.foi.techtitans.ttpay.login_modular.activity_login
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
+import hr.foi.techtitans.ttpay.R
+import hr.foi.techtitans.ttpay.login_modular.model_login.LoginAdapterPIN
+import hr.foi.techtitans.ttpay.navigationBar.activity_navigationBar.MainActivity
+
+class LoginWithPIN : AppCompatActivity() {
+
+    private lateinit var editTextPIN:EditText
+    private lateinit var btnLogin:Button
+    private lateinit var btnBack:ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_with_pin)
+
+        editTextPIN = findViewById(R.id.digitCodePIN)
+        btnLogin=findViewById(R.id.btn_login_PIN_activity)
+        btnBack=findViewById(R.id.imgBackButton)
+
+
+        btnBack.setOnClickListener{
+            val main= Intent(this, MainActivity::class.java)
+            startActivity(main)
+            finish()
+        }
+
+        btnLogin.setOnClickListener{
+
+        }
+
+
+    }
+}
