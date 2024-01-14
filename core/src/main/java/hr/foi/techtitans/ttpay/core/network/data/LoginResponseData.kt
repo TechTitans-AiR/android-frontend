ba19ca504f30e29640c1576bb9cb5c10831eea40
lgrzelj <lgrzelj@student.foi.hr>
2024-01-14 19:22:39 +0100

    Implemented logic for using renamed ResponseBodyData and added ResponseBodyData to the endpoint as response.  (TT-93)

--- core/src/main/java/hr/foi/techtitans/ttpay/core/network/data/LoginResponseData.kt
+++ core/src/main/java/hr/foi/techtitans/ttpay/core/network/data/LoginResponseData.kt
@@ -2,12 +2,12 @@ package hr.foi.techtitans.ttpay.core.network.data
 
 data class LoginResponseData(
     val headers: Map<String, String>,
-    val body: ResponseBody,
+    val body: ResponseBodyData,
     val statusCode: String,
     val statusCodeValue: Int
 )
 
-data class ResponseBody(
+data class ResponseBodyData(
     val message: String,
     val token: String
 )
