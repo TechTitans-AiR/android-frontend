cddb14a1824e82cc503eec64d9808ed3ea75de15
lgrzelj <lgrzelj@student.foi.hr>
2024-01-14 19:17:19 +0100

    Changed name of progress bar. Also renamed class Body in core module do that it can be used for module login via PIN for data that will be received from server. (TT-93)

--- core/src/main/java/hr/foi/techtitans/ttpay/core/network/data/LoginResponseData.kt
+++ core/src/main/java/hr/foi/techtitans/ttpay/core/network/data/LoginResponseData.kt
@@ -2,12 +2,12 @@ package hr.foi.techtitans.ttpay.core.network.data
 
 data class LoginResponseData(
     val headers: Map<String, String>,
-    val body: Body,
+    val body: ResponseBody,
     val statusCode: String,
     val statusCodeValue: Int
 )
 
-data class Body(
+data class ResponseBody(
     val message: String,
     val token: String
 )
